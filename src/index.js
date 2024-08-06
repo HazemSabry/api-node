deadf03210853309949a3e19e4b8543b6b6cd4ea
Hazem Sabry <89419013+HazemSabry@users.noreply.github.com>
2024-08-07 01:39:59 +0300

    Finish the docker File

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// setup the logger
+app.use(morgan('tiny'));
+
+app.get('/', async (req, res) => {
+    res.send('response');
+});
+
+app.get('/ping', async (_, res) => {
+    res.send('pong');
+});
+
+const server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+process.on('SIGTERM', () => {
+    console.debug('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+        console.debug('HTTP server closed');
+    });
+});
